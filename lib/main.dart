896f9d7e9646d953bda30e1ee8bb4e51b32a5b31
pr47h4m <pr47h4m@gmail.com>
2021-03-27 10:34:19 +0530

    Created Simple Counter App

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,47 @@ void main() => runApp(FlutterGeneral());
 class FlutterGeneral extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.system,
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int counter = 0;
+  void incrementCounter() {
+    setState(() {
+      ++counter;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Flutter General",
+        ),
+      ),
+      body: Center(
+        child: Text(
+          "$counter",
+          style: Theme.of(context).textTheme.headline1,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: incrementCounter,
+      ),
+    );
   }
 }
